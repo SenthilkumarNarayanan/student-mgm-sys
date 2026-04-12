64596d5152f9d474bc89470069bfce7c5ebf740b
SenthilkumarNarayanan <nsk89profile@gmail.com>
2026-04-13 01:33:17 +0530

    cors fixed

--- backend/app.js
+++ backend/app.js
@@ -7,26 +7,7 @@ const studentRoutes = require('./routes/studentRoutes');
 const app = express();
 
 // CORS configuration
-const allowedOrigins = [
-  "https://student-mgm-sys-frontend.onrender.com"
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin (Postman, mobile apps)
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("CORS not allowed"));
-    }
-  },
-  credentials: true
-}));
-
-app.options("/*", cors());
-
+app.use(cors());
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/admin', adminRoutes);
